4304db149c5fd000d39e74f16c22c9e9223dc749
oluwadare <wintan1418@gmail.com>
2021-08-24 22:04:26 +0100

    add table for books category and title

---
+++ src/component/Book.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Book = ({
+  bookID,
+  title,
+  category,
+}) => (
+  <tr>
+    <td>{bookID}</td>
+    <td>{title}</td>
+    <td>{category}</td>
+  </tr>
+);
+Book.propTypes = {
+  bookID: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
+  // handleRemoveBook: PropTypes.func.isRequired,
+};
+
+export default Book;
