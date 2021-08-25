6e6d95d16829536b025b7ac88909694c47c05d91
oluwadare <wintan1418@gmail.com>
2021-08-25 15:34:17 +0100

    add table for book list and call id methods and params

--- src/containers/BookList.js
+++ src/containers/BookList.js
@@ -1,4 +1,25 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import Book from './../component/Book';
+import Book from '../component/Book';
+
+const BookList = ({ books }) => (
+  <table>
+    <tr>
+      <th>BookID</th>
+      <th>Title</th>
+      <th>Category</th>
+    </tr>
+    {
+    books.map(book => (
+      <Book
+        bookID={book.bookID}
+        key={book.bookID}
+        title={book.title}
+        category={book.category}
+      />
+    ))
+  }
+  </table>
+
+);
