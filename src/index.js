678062265c385bc3aea2be140b0c5529be1a86de
root <root@DESKTOP-2S2UDLA.localdomain>
2021-08-26 11:14:34 +0100

    implement change requested by tse

--- src/index.js
+++ src/index.js
@@ -4,9 +4,26 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import App from './component/App';
 import rootReducer from './reducers';
+import { generateRandomNumber, categories } from './list';
+
+const INITIAL_STATE = {
+  defaultBooks: [
+    {
+      bookID: generateRandomNumber(),
+      title: 'SHERLOCK OF HOLMES',
+      category: categories[generateRandomNumber(categories.length)],
+    },
+    {
+      bookID: generateRandomNumber(),
+      title: 'INTO THE MULTIVERSE',
+      category: categories[generateRandomNumber(categories.length)],
+    },
+
+  ],
+};
 
 const store = createStore(
-  rootReducer,
+  rootReducer, INITIAL_STATE,
 );
 
 ReactDOM.render(
