c190c3a3da994b83743c82a953eb8737f1ce8f68
oluwadare <wintan1418@gmail.com>
2021-08-27 12:04:29 +0100

    add code to generate table format and actions

--- src/containers/BookList.js
+++ src/containers/BookList.js
@@ -2,15 +2,34 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Book from '../component/Book';
+import { removeBook } from '../action';
+import { generateRandomNumber } from '../list';
 
-const BookList = ({ books }) => (
-  <table>
-    <tr>
-      <th>BookID</th>
-      <th>Title</th>
-      <th>Category</th>
-    </tr>
-    {
+const BookList = ({ books, deleteBook }) => {
+  const handleRemoveBook = id => deleteBook(id);
+
+  const allBooks = books.map(book => (
+    <Book
+      id={generateRandomNumber()}
+      handleRemoveBook={handleRemoveBook}
+      book={book}
+      key={generateRandomNumber()}
+    />
+  ));
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>BookID</th>
+          <th>Title</th>
+          <th>Category</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        {allBooks}
+      </tbody>
+      {
     books.map(book => (
       <Book
         bookID={book.bookID}
@@ -20,19 +39,22 @@ const BookList = ({ books }) => (
       />
     ))
   }
-  </table>
+    </table>
 
-);
+  );
+};
+
 BookList.propTypes = {
-  books: PropTypes.arrayOf(PropTypes.object),
+  books: PropTypes.arrayOf(PropTypes.object).isRequired,
+  deleteBook: PropTypes.func.isRequired,
 };
 
-BookList.defaultProps = {
-  books: [],
-};
-
-const mapStateToProps = ({ books }) => ({
-  books,
+const mapStateToProps = state => ({
+  books: state.books,
 });
 
-export default connect(mapStateToProps)(BookList);
+const mapDispatchToProps = dispatch => ({
+  deleteBook: id => dispatch(removeBook(id)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
