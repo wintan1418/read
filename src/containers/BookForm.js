e5374273c2af0d879fa3a98ac99a8e2cba34fc53
oluwadare <wintan1418@gmail.com>
2021-08-27 11:01:19 +0100

    add if statements for error

--- src/containers/BookForm.js
+++ src/containers/BookForm.js
@@ -18,6 +18,23 @@ const initialValue = {
       {category}
     </option>
   ));
+  const handleChange = b => {
+    const { value } = b.target;
+    setInput({ ...input, [e.target.name]: value.toUpperCase() });
+  }; const validateInputError = () => {
+    const { title, category } = input;
+    let error = '';
+    if (!title) {
+      error = 'You must need give a Title,please';
+    } else if (title.length < 5) {
+      error = 'Title length too short,input no less than five character';
+    } else if (!category) {
+      error = 'You must need give the category of this book';
+    } else if (category.length < 3) {
+      error = 'Too short!';
+    }
+    return error;
+  };
   return (
     <div>
       <form action="">
