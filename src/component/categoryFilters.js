dc1c27571570997713e6b4557992ab491ba50a89
oluwadare <wintan1418@gmail.com>
2021-08-30 14:28:17 +0100

    add code and map logic through categories

--- src/component/categoryFilters.js
+++ src/component/categoryFilters.js
@@ -1,2 +1,13 @@
 import PropTypes from 'prop-types';
-import {categories} from '../list';
+import { categories } from '../list';
+
+function CategoryFilter({ hamdleFilter }) {
+  return (
+    <div>
+      <span>Filter through the Category: </span>
+      <select name="categoryFilter" id="categoryFilter" onChange={handleFilter}>
+        {['ALL', ...categories].map(category => <option key={category}>{category}</option>)}
+      </select>
+    </div>
+  );
+}
