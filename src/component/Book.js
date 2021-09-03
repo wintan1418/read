2b2c6deb4bd2b5f64c4eb952a83c22c4954d5ec7
root <root@DESKTOP-2S2UDLA.localdomain>
2021-09-03 11:14:12 +0100

    refactor book.js for stylings

--- src/component/Book.js
+++ src/component/Book.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Book.css';
 
 const Book = ({
   bookID,
@@ -12,13 +13,13 @@ const Book = ({
       <div>
         <div className="category">{category}</div>
         <div className="title">{title}</div>
-        <div className="author">author X</div>
+        <div className="author">AUTHOR X</div>
         <div className="target-category">{ category }</div>
       </div>
       <div className="actions">
         <button
           type="button"
-          >
+        >
           comments
         </button>
         <button
@@ -33,27 +34,34 @@ const Book = ({
             handleRemoveBook(book);
           }}
         >
-  <tr>
-    <td>{bookID}</td>
-    <td>{title}</td>
-    <td>{category}</td>
-    <td>
-      <button
-        onClick={() => {
-          const book = {
-            bookID,
-            title,
-            category,
-          };
-
-          handleRemoveBook(book);
-        }}
-        type="button"
-      >
-        Remove
-      </button>
-    </td>
-  </tr>
+          Remove
+        </button>
+        <button
+          type="button"
+        >
+          Edits
+        </button>
+      </div>
+    </div>
+    <div className="complete-container">
+      <div className="complete">
+        <div className="circle" />
+        <div className="complete-value">
+          <span
+            className="percent"
+          >
+            {`${Math.floor(Math.random() * 100)}%`}
+          </span>
+          <span className="faded-text">Completed</span>
+        </div>
+      </div>
+      <div className="update">
+        <span className="title">CURRENT CHAPTER</span>
+        <span className="chapter">{`Chapter ${Math.floor(Math.random() * 34 + 1)}`}</span>
+        <button type="button">UPDATE PROGRESS</button>
+      </div>
+    </div>
+  </div>
 );
 
 Book.propTypes = {
